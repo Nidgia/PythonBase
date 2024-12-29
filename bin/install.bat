39be866832ab070a35b4e106fc74e5893d691b77
Nidgia Maria Nicolodi <nidgia00maria@gmail.com>
2024-12-29 12:07:44 -0300

    first commit

---
+++ bin/install.bat
@@ -0,0 +1,9 @@
+cd ..\
+
+python -m venv virtualized
+
+call .\virtualized\Scripts\activate.bat
+
+pip install -r .\bin\requirements.txt
+
+deactivate
---
+++ bin/start.bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd ..\
+
+call .\virtualized\Scripts\activate.bat
+
+cmd /k
